50ccabfaddf6a45c7d46a9daf35909a836bff9d0
Remi Gau <remi_gau@hotmail.com>
2023-05-20 14:51:15 -0400

    fix test

--- test_utils.py
+++ test_utils.py
@@ -11,19 +11,19 @@ def input_tsv():
 
 
 def test_read_csv(input_tsv):
-    df = read_csv(input_tsv, sep="\t")
+    df = read_csv_autodetect_date(input_tsv, sep="\t")
     assert df.acq_date.dtype == "datetime64[ns]"
 
 
 def test_is_yes_no(input_tsv):
-    df = read_csv(input_tsv, sep="\t")
+    df = read_csv_autodetect_date(input_tsv, sep="\t")
 
     assert ~is_yes_no(df["acq_date"])
     assert is_yes_no(df["yes_no_nan"])
 
 
 def test_is_euro_format(input_tsv):
-    df = read_csv(input_tsv, sep="\t")
+    df = read_csv_autodetect_date(input_tsv, sep="\t")
 
     assert ~is_euro_format(df["acq_date"])
     assert ~is_euro_format(df["participant_id"])
