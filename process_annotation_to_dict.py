9493d8fe75c6405e6df311521c10b73e95d63a2a
Sebastian Urchs <surchs@users.noreply.github.com>
2023-06-18 15:02:03 -0400

    [FIX] Give age columns a Transformation key (#18)
    * Unit test for getting the transform heuristic
    
    * Ran black
    
    * test that transformation gets added to description
    
    * Get the transformations into the data
    
    * Update process_annotation_to_dict.py
    
    Co-authored-by: Alyssa Dai <alyssa.dai@mail.mcgill.ca>
    
    * Fix docstring
    
    * PR review fixes
    
    - test rename
    - refactor isAbout maker
    - return nothing when continuous has no transformation
    - update bad continuous test now that the dict will have no annotations
      at all
    
    * Complete refactoring the isAbout maker
    
    * Add handler for participantID columns
    
    * Updating bagel-schema
    
    * Fix a problem with every column being a tool column
    
    * Apply suggestions from code review
    
    Co-authored-by: Alyssa Dai <alyssa.dai@mail.mcgill.ca>
    
    ---------
    
    Co-authored-by: Alyssa Dai <alyssa.dai@mail.mcgill.ca>

--- process_annotation_to_dict.py
+++ process_annotation_to_dict.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import json
+from typing import Tuple
 
 import jsonschema
 import pandas as pd
@@ -12,7 +13,7 @@ with (MYPATH / "bagel_dictionary_schema.json").open("r") as f:
 
 def is_discrete(df: pd.DataFrame) -> bool:
     """True if each row in dataframe describes a discrete value in a column."""
-    return  not df.is_row.all()
+    return not df.is_row.all()
 
 
 def is_dropped(df: pd.DataFrame) -> bool:
@@ -20,8 +21,12 @@ def is_dropped(df: pd.DataFrame) -> bool:
     return (get_col_rows(df)["Decision"] == "drop").item()
 
 
+def is_identifying(df: pd.DataFrame) -> bool:
+    return get_col_rows(df)["controlled_term"].item() == "nb:ParticipantID"
+
+
 def is_tool(df: pd.DataFrame) -> bool:
-    return get_col_rows(df)["isPartOf"].notna().item()
+    return "cogatlas:" in str(get_col_rows(df)["isPartOf"].item())
 
 
 def get_ds_path(dataset: str) -> Path:
@@ -29,7 +34,7 @@ def get_ds_path(dataset: str) -> Path:
 
 
 def get_dict_path(dataset: str) -> Path:
-    return get_ds_path(dataset) / "participants.json";
+    return get_ds_path(dataset) / "participants.json"
 
 
 def fetch_data_dictionary(dataset: str) -> dict:
@@ -39,19 +44,23 @@ def fetch_data_dictionary(dataset: str) -> dict:
         return data_dict
     else:
         return {}
-    
-    
-def describe_continuous(df: pd.DataFrame) -> dict:
-    return {
-        "Annotations": {
-            "IsAbout": {
-                "TermURL": get_col_rows(df)["controlled_term"].item(),
-                "Label": ""
-            }
-        }
-    }
-    
-    
+
+
+def get_transform_heuristic(df: pd.DataFrame) -> Tuple[str]:
+    """Returns Neurobagel transformation term and short label from parsed type"""
+    col_type = get_col_rows(df)["type"].item()
+    if col_type == "float64":
+        return ("nb:float", "float data")
+    if col_type == "int64":
+        return ("nb:int", "integer data")
+    if col_type == "nb:bounded":
+        return ("nb:bounded", "bounded data")
+    if col_type == "nb:euro":
+        return ("nb:euro", "european decimal value")
+    else:
+        return ("", "")
+
+
 def get_col_rows(df: pd.DataFrame) -> pd.DataFrame:
     return df.query("is_row == True")
 
@@ -59,37 +68,67 @@ def get_col_rows(df: pd.DataFrame) -> pd.DataFrame:
 def get_level_rows(df: pd.DataFrame) -> pd.DataFrame:
     return df.query("is_row == False")
 
-    
-def describe_level(term: str) -> dict:
-    return {
-            "TermURL": term,
-            "Label": ""
-        }
 
+def describe_isabout(term: str) -> dict:
+    return {            
+            "IsAbout": {
+                "TermURL": term,
+                "Label": "",
+            }
+    }
     
+    
+def describe_identified(df: pd.DataFrame) -> dict:
+    return {
+        "Annotations": {
+        **describe_isabout(get_col_rows(df)["controlled_term"].item()),
+        "Identifies": "participant"
+        }
+    }
+
+
+def describe_level(term: str) -> dict:
+    return {"TermURL": term, "Label": ""}
+
+
+def describe_continuous(df: pd.DataFrame) -> dict:
+    t_url, t_label = get_transform_heuristic(df)
+    if not t_url:
+        print(df["dataset"].item(),  "has no age")
+        return {}
+    
+    return {
+        "Annotations": {
+            **describe_isabout(get_col_rows(df)["controlled_term"].item()),
+            "Transformation": {"TermURL": t_url, "Label": t_label}
+        }
+    }
+
+
 def describe_discrete(df: pd.DataFrame) -> dict:
     return {
         "Annotations": {
-            "IsAbout": {
-                "TermURL": get_col_rows(df)["controlled_term"].item(),
-                "Label": ""
-            },
+            **describe_isabout(get_col_rows(df)["controlled_term"].item()),
             "Levels": {
-                row.value: describe_level(row.controlled_term) for _, row in get_level_rows(df).iterrows()
-            }
+                row.value: describe_level(row.controlled_term)
+                for _, row in get_level_rows(df).iterrows()
+            },
         }
     }
-    
+
 
 def describe_tool(df: pd.DataFrame) -> dict:
     return {
-        "IsPartOf": {
-            "TermURL": get_col_rows(df)["isPartOf"].item(),
-            "Label": ""
+        "Annotations": {
+            **describe_isabout(get_col_rows(df)["controlled_term"].item()),
+            "IsPartOf": {
+                "TermURL": get_col_rows(df)["isPartOf"].item(),
+                "Label": "",
+            },
         }
     }
-    
-    
+
+
 def add_description(data_dict: dict) -> dict:
     """
     Given a column, adds an empty description if none is present.
@@ -97,13 +136,14 @@ def add_description(data_dict: dict) -> dict:
     """
     # TODO: This function is a hacky fix for bad data dictionaries in the input data and should be removed
     for key, column in data_dict.items():
-        
-        if not "Description" in column.keys():
-            column["Description"] = "There should have been a description here, but there wasn't. :("
+        if "Description" not in column.keys():
+            column[
+                "Description"
+            ] = "There should have been a description here, but there wasn't. :("
         data_dict[key].update(**column)
     return data_dict
-    
-    
+
+
 def is_valid_dict(data_dict: dict) -> bool:
     """Returns True for valid Neurobagel data dictionary"""
     try:
@@ -111,51 +151,53 @@ def is_valid_dict(data_dict: dict) -> bool:
         return True
     except jsonschema.ValidationError:
         return False
-    
-    
+
+
 def write_data_dict(data_dict: dict, path: Path, name: str) -> None:
     path.mkdir(exist_ok=True)
     with (path / f"{name}.json").open("w") as f:
         json.dump(data_dict, f, indent=2)
-    
+
 
 def process_dict(ds_df: pd.DataFrame, user_dict: dict) -> dict:
     """
-    Take an existing data dictionary (can be empty) and 
+    Take an existing data dictionary (can be empty) and
     add what we have to it so that it gets more detailed.
     """
     for col, col_df in ds_df.groupby("column"):
-            
         if is_dropped(col_df):
             continue
-        if is_discrete(col_df):
-            user_dict.setdefault(col, {}).update(**describe_discrete(col_df))    
+        if is_identifying(col_df):
+            user_dict.setdefault(col, {}).update(**describe_identified(col_df))
+        elif is_discrete(col_df):
+            user_dict.setdefault(col, {}).update(**describe_discrete(col_df))
+        elif is_tool(col_df):
+            user_dict.setdefault(col, {}).update(**describe_tool(col_df))
         else:
             user_dict.setdefault(col, {}).update(**describe_continuous(col_df))
-        
-        if is_tool(col_df):
-            user_dict[col].setdefault("Annotations", {}).update(**describe_tool(col_df))
-        
-    user_dict = add_description(data_dict=user_dict)
-    
-    return user_dict
 
+    user_dict = add_description(data_dict=user_dict)
+
+    return user_dict
 
 
 def main():
     annotated = pd.read_csv(MYPATH / "outputs/annotated_levels.tsv", sep="\t")
-    
+
     for dataset, ds_df in annotated.groupby("dataset"):
-        
         data_dict = fetch_data_dictionary(dataset=dataset)
-        
+
         data_dict = process_dict(ds_df, data_dict)
-        
+
         if not is_valid_dict(data_dict):
             # TODO: make smarter choices about logging and warnings
-            print("Uhoh, this is not a valid dict", dataset)
-        write_data_dict(data_dict, MYPATH / "outputs/data_dictionaries/", name=dataset)
+            # print("Uhoh, this is not a valid dict", dataset)
+            pass
+        write_data_dict(
+            data_dict, MYPATH / "outputs/data_dictionaries/", name=dataset
+        )
     print("Tada!")
+
 
 if __name__ == "__main__":
     main()
--- tests_proc_dict.py
+++ tests_proc_dict.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import pytest
 
-from process_annotation_to_dict import process_dict
+from process_annotation_to_dict import process_dict, get_transform_heuristic, describe_continuous
 
 
 @pytest.fixture
@@ -26,6 +26,21 @@ def discrete_annotation():
         "isPartOf": {1: "", 2: "", 3: "", 4: ""},
         "Decision": {1: "keep", 2: "keep", 3: "keep", 4: "keep"},
     }
+    
+    
+@pytest.fixture
+def participant_annotation():
+    return {
+        "dataset": {10: "ds000002"},
+        "column": {10: "participant_id"},
+        "type": {10: "str"},
+        "value": {10: ""},
+        "is_row": {10: True},
+        "description": {10: ""},
+        "controlled_term": {10: "nb:ParticipantID"},
+        "isPartOf": {10: ""},
+        "Decision": {10: "keep"},
+    }
 
 
 @pytest.fixture
@@ -33,6 +48,7 @@ def continuous_annotation():
     return {
         "dataset": {10: "ds000002"},
         "column": {10: "age"},
+        "type": {10: "float64"},
         "value": {10: ""},
         "is_row": {10: True},
         "description": {10: ""},
@@ -65,10 +81,7 @@ def drop_annotation(continuous_annotation):
 @pytest.fixture
 def user_dict():
     return {
-        "age": {
-            "Description": "age of the participant",
-            "Units": "years"
-        },
+        "age": {"Description": "age of the participant", "Units": "years"},
         "sex": {
             "Description": "gender of the participant",
             "Levels": {"M": "male", "F": "female"},
@@ -76,24 +89,89 @@ def user_dict():
     }
 
 
-def test_original_data_unchanged_when_no_annotation(drop_annotation, user_dict):
+def test_original_data_unchanged_when_no_annotation(
+    drop_annotation, user_dict
+):
     """Dropped annotations should not be added to the generated data dictionary"""
     data = pd.DataFrame(drop_annotation)
     result = process_dict(data, user_dict)
     assert result == user_dict
 
 
-def test_original_data_augmented_by_annotation(continuous_annotation, user_dict):
+def test_original_data_augmented_by_annotation(
+    continuous_annotation, user_dict
+):
     data = pd.DataFrame(continuous_annotation)
     result = process_dict(data, user_dict)
-    
+
     assert result.get("age", {}).get("Annotations") is not None
     assert result.get("sex") is not None
-    
-    
+
+
+def test_good_continuous_has_transformation(continuous_annotation, user_dict):
+    data = pd.DataFrame(continuous_annotation)
+    result = process_dict(data, user_dict)
+
+    assert (
+        result.get("age").get("Annotations").get("Transformation") is not None
+    )
+
+
+def test_bad_continuous_lacks_transformation(continuous_annotation, user_dict):
+    continuous_annotation.update(**{"type": {10: "nonsense_heuristic"}})
+    data = pd.DataFrame(
+        continuous_annotation
+    )
+    result = process_dict(data, user_dict)
+
+    assert result.get("age").get("Annotations") is None
+
+
+def test_describe_continuous(continuous_annotation):
+    result = describe_continuous(pd.DataFrame(continuous_annotation))
+    assert result == {"Annotations": {
+        "IsAbout": {
+            "TermURL": "nb:Age",
+            "Label": "",
+        },
+        "Transformation": {
+            "TermURL": "nb:float",
+            "Label": "float data",
+            },
+    }}
+
+
 def test_partof_annotation_is_processed(tool_annotation, user_dict):
     data = pd.DataFrame(tool_annotation)
     result = process_dict(data, user_dict)
-    
+
     assert result.get("tool1") is not None
-    assert result.get("tool1").get("Annotations", {}).get("IsPartOf") is not None
+    assert (
+        result.get("tool1").get("Annotations", {}).get("IsPartOf") is not None
+    )
+
+
+@pytest.mark.parametrize(
+    "annotation,expected",
+    [
+        ({"type": {10: "float64"}}, "nb:float"),
+        ({"type": {10: "int64"}}, "nb:int"),
+        ({"type": {10: "nb:bounded"}}, "nb:bounded"),
+        ({"type": {10: "nb:euro"}}, "nb:euro"),
+    ],
+)
+def test_get_transform_heuristic(annotation, expected, continuous_annotation):
+    continuous_annotation.update(**annotation)
+    df = pd.DataFrame(continuous_annotation)
+    result = get_transform_heuristic(df)
+    assert result[0] == expected
+    
+    
+def test_participant_id_column_goes_through(participant_annotation, user_dict):
+    df = pd.DataFrame(participant_annotation)
+    result = process_dict(df, user_dict)
+    assert result.get("participant_id") is not None
+    assert result.get("participant_id").get("Annotations") is not None
+    assert result.get("participant_id").get("Annotations").get("Identifies") is not None
+
+
