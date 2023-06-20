7264a2495ec6bf0a1b99dda6176f65376d56743d
Alyssa Dai <alyssa.ydai@gmail.com>
2023-06-20 17:35:26 -0400

    provide portal URI in bagel pheno call

--- run_bagel_cli.sh
+++ run_bagel_cli.sh
@@ -8,6 +8,7 @@ ldout=outputs/openneuro-jsonld/
 
 ds=$1
 ds_name=$2
+ds_portal=https://openneuro.org/datasets/${ds}
 workdir=`realpath ${ldin}/$ds`
 out=(${ldout}/${ds}.jsonld)
 
@@ -23,8 +24,8 @@ echo $ds $ds_name
 if [ ! -e ${out} ]; then
 
 
-    echo bagel pheno --pheno ${workdir}/participants.tsv --dictionary ${workdir}/participants.json --output ${workdir} --name $ds_name
-    docker run -v ${workdir}:${workdir} neurobagel/bagelcli:latest pheno --pheno ${workdir}/participants.tsv --dictionary ${workdir}/participants.json --output ${workdir} --name $ds_name
+    echo bagel pheno --pheno ${workdir}/participants.tsv --dictionary ${workdir}/participants.json --output ${workdir} --name $ds_name --portal $ds_portal
+    docker run -v ${workdir}:${workdir} neurobagel/bagelcli:latest pheno --pheno ${workdir}/participants.tsv --dictionary ${workdir}/participants.json --output ${workdir} --name $ds_name --portal $ds_portal
     docker run -v ${workdir}:${workdir} neurobagel/bagelcli:latest bids --jsonld-path ${workdir}/pheno.jsonld  --bids-dir ${workdir} --output ${workdir}
 
     cp ${workdir}/pheno_bids.jsonld ${out}
