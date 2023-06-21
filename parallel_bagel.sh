2f8650421dba2ad41188d36fa37093ed1627131c
Alyssa Dai <alyssa.ydai@gmail.com>
2023-06-21 11:59:54 -0400

    prevent dataset name from being interpreted as multiple args

--- parallel_bagel.sh
+++ parallel_bagel.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+ldout=outputs/openneuro-jsonld/
+
 for ds in inputs/openneuro-jsonld/*; do
     ds_id=$(basename $ds)
-    if [ ! -e outputs/openneuro-jsonld/${ds_id}.jsonld ]; then
+    if [ ! -e ${ldout}/${ds_id}.jsonld ]; then
         # Get human-readable dataset name or "None"
         ds_name=$(python extract_bids_dataset_name.py --ds $ds)
-        echo $ds_id $ds_name
+
+        echo ./run_bagel_cli.sh $ds_id \"$ds_name\"
     fi
 
 # Now run this in parallel with -j 8 jobs
-done | parallel -j 8 ./run_bagel_cli.sh {}
+# and have stderr and output both displayed and appended to a file
+done | parallel -j 8 2>&1 | tee -a ${ldout}/log.txt
--- run_bagel_cli.sh
+++ run_bagel_cli.sh
@@ -6,8 +6,8 @@ docker pull neurobagel/bagelcli:latest
 ldin=inputs/openneuro-jsonld/
 ldout=outputs/openneuro-jsonld/
 
-ds=$1
-ds_name=$2
+ds="$1"
+ds_name="$2"
 ds_portal=https://openneuro.org/datasets/${ds}
 workdir=`realpath ${ldin}/$ds`
 out=(${ldout}/${ds}.jsonld)
@@ -20,12 +20,12 @@ if [ ! -e ${ldout} ]; then
     mkdir -p ${ldout}
 fi
 
-echo $ds $ds_name
+echo $ds "$ds_name"
 if [ ! -e ${out} ]; then
 
 
-    echo bagel pheno --pheno ${workdir}/participants.tsv --dictionary ${workdir}/participants.json --output ${workdir} --name $ds_name --portal $ds_portal
-    docker run -v ${workdir}:${workdir} neurobagel/bagelcli:latest pheno --pheno ${workdir}/participants.tsv --dictionary ${workdir}/participants.json --output ${workdir} --name $ds_name --portal $ds_portal
+    echo bagel pheno --pheno ${workdir}/participants.tsv --dictionary ${workdir}/participants.json --output ${workdir} --name "$ds_name" --portal $ds_portal
+    docker run -v ${workdir}:${workdir} neurobagel/bagelcli:latest pheno --pheno ${workdir}/participants.tsv --dictionary ${workdir}/participants.json --output ${workdir} --name "$ds_name" --portal $ds_portal
     docker run -v ${workdir}:${workdir} neurobagel/bagelcli:latest bids --jsonld-path ${workdir}/pheno.jsonld  --bids-dir ${workdir} --output ${workdir}
 
     cp ${workdir}/pheno_bids.jsonld ${out}
