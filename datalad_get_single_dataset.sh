a1e388c619729b74efaecc6195a51c196887c828
maelleF <maelle.freteault@gmail.com>
2023-07-21 12:11:37 -0400

    replace run_bagel_cli by commands/change var name

--- datalad_get_single_dataset.sh
+++ datalad_get_single_dataset.sh
@@ -2,18 +2,21 @@
 
 #step1
 ds_id="ds004107"
+ds_portal="https://openneuro.org/datasets/${ds_id}"
 GIT="git@github.com:OpenNeuroDatasets-JSONLD/${ds_id}"
 OUTDIR="/home/maellef/DataBase/NeuroBagel_test/"
 
-OUTPATH=${OUTDIR}${ds_id}
+workdir=${OUTDIR}${ds_id}
 
-datalad clone ${GIT} ${OUTPATH}
-datalad get -d $OUTPATH "${OUTPATH}/participants.tsv"
-datalad get -d $OUTPATH "${OUTPATH}/participants.json"
-datalad get -d $OUTPATH "${OUTPATH}/dataset_description.json"
+datalad clone ${GIT} ${workdir}
+datalad get -d $workdir "${workdir}/participants.tsv"
+datalad get -d $workdir "${workdir}/participants.json"
+datalad get -d $workdir "${workdir}/dataset_description.json"
 
-ds_name=$(python extract_bids_dataset_name.py --ds $OUTPATH)
+ds_name=$(python extract_bids_dataset_name.py --ds $workdir)
 if [ "$ds_name" == "None" ] ; then ds_name=$ds_id ; else ds_name=$ds_name ; fi
 
 #step2
-echo ./run_bagel_cli.sh $ds_id \"$ds_name\"
+bagel pheno --pheno ${workdir}/participants.tsv --dictionary ${workdir}/participants.json --output ${workdir} --name "$ds_name" --portal $ds_portal
+bagel bids --jsonld-path ${workdir}/pheno.jsonld  --bids-dir ${workdir} --output ${workdir}
+
