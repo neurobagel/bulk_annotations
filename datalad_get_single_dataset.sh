b25396d095461143787920b6a1a4274503e8a262
maelleF <maelle.freteault@gmail.com>
2023-07-21 12:24:50 -0400

    add output directory for jsonld file/fix var name

--- datalad_get_single_dataset.sh
+++ datalad_get_single_dataset.sh
@@ -2,11 +2,15 @@
 
 #step1
 ds_id="ds004107"
+
 ds_portal="https://openneuro.org/datasets/${ds_id}"
 GIT="git@github.com:OpenNeuroDatasets-JSONLD/${ds_id}"
-OUTDIR="/home/maellef/DataBase/NeuroBagel_test/"
+bagel_dir="/home/maellef/DataBase/NeuroBagel_test/"
+ldout="${bagel_dir}outputs/openneuro-jsonld/"
+mkdir -p $ldout
+out="${ldout}${ds_id}.jsonld"
 
-workdir=${OUTDIR}${ds_id}
+workdir="${bagel_dir}${ds_id}"
 
 datalad clone ${GIT} ${workdir}
 datalad get -d $workdir "${workdir}/participants.tsv"
@@ -19,4 +23,4 @@ if [ "$ds_name" == "None" ] ; then ds_name=$ds_id ; else ds_name=$ds_name ; fi
 #step2
 bagel pheno --pheno ${workdir}/participants.tsv --dictionary ${workdir}/participants.json --output ${workdir} --name "$ds_name" --portal $ds_portal
 bagel bids --jsonld-path ${workdir}/pheno.jsonld  --bids-dir ${workdir} --output ${workdir}
-
+cp ${workdir}/pheno_bids.jsonld ${out}
