a2c2ee4e18028d32183daabd1f9a643e86e1f3f7
maelleF <maelle.freteault@gmail.com>
2023-07-21 11:52:29 -0400

    add cmd to run_bagel_cli + change variable name

--- datalad_get_single_dataset.sh
+++ datalad_get_single_dataset.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 #step1
-ID="ds004107"
-GIT="git@github.com:OpenNeuroDatasets-JSONLD/${ID}"
+ds_id="ds004107"
+GIT="git@github.com:OpenNeuroDatasets-JSONLD/${ds_id}"
 OUTDIR="/home/maellef/DataBase/NeuroBagel_test/"
 
-OUTPATH=${OUTDIR}${ID}
+OUTPATH=${OUTDIR}${ds_id}
 
 datalad clone ${GIT} ${OUTPATH}
 datalad get -d $OUTPATH "${OUTPATH}/participants.tsv"
@@ -13,9 +13,7 @@ datalad get -d $OUTPATH "${OUTPATH}/participants.json"
 datalad get -d $OUTPATH "${OUTPATH}/dataset_description.json"
 
 ds_name=$(python extract_bids_dataset_name.py --ds $OUTPATH)
-if [ "$ds_name" == "None" ] ; then ds_name=$ID ; else ds_name=$ds_name ; fi
-
-echo $ds_name
+if [ "$ds_name" == "None" ] ; then ds_name=$ds_id ; else ds_name=$ds_name ; fi
 
 #step2
-
+echo ./run_bagel_cli.sh $ds_id \"$ds_name\"
