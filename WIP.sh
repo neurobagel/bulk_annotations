92a6f16dc29f587b92e9cbc990ff4ab7f5c4e07d
maelleF <maelle.freteault@gmail.com>
2023-07-21 11:16:18 -0400

    add "datalad get <file>"

--- WIP.sh
+++ WIP.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
 #step1
-ID="ds004393"
+ID="ds004107"
 GIT="git@github.com:OpenNeuroDatasets-JSONLD/${ID}"
-
 OUTDIR="/home/maellef/DataBase/NeuroBagel_test/"
+
 OUTPATH=${OUTDIR}${ID}
 
 datalad clone ${GIT} ${OUTPATH}
-#datalad get "${OUTPATH}/participants.json"
-ds_name=$(python extract_bids_dataset_name.py --ds $OUTPATH)
-if [ $ds_name == "None" ] ; then ds_name=$ID ; fi
+datalad get -d $OUTPATH "${OUTPATH}/participants.tsv"
+datalad get -d $OUTPATH "${OUTPATH}/participants.json"
+datalad get -d $OUTPATH "${OUTPATH}/dataset_description.json"
 
-echo ${ds_name}
+ds_name=$(python extract_bids_dataset_name.py --ds $OUTPATH)
+if [ "$ds_name" == "None" ] ; then ds_name=$ID ; else ds_name=$ds_name ; fi
+
+echo $ds_name
 
 #step2
+
