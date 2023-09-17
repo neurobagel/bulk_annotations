755c52b4106521b3b022df37b0a0b52a690e2018
Alyssa Dai <alyssa.ydai@gmail.com>
2023-09-17 12:34:04 -0400

    update comments and pull latest CLI image

--- datalad_get_single_dataset.sh
+++ datalad_get_single_dataset.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-#step1
 ds_id=$1
 
+docker pull neurobagel/bagelcli:latest
+
+# Get the data
 ds_portal="https://github.com/OpenNeuroDatasets-JSONLD/${ds_id}.git"
 ds_git="git@github.com:OpenNeuroDatasets-JSONLD/${ds_id}"
 
@@ -19,10 +21,11 @@ datalad get -d $workdir "${workdir}/participants.tsv"
 datalad get -d $workdir "${workdir}/participants.json"
 datalad get -d $workdir "${workdir}/dataset_description.json"
 
+# Get the dataset label
 ds_name=$(python extract_bids_dataset_name.py --ds $workdir)
 if [ "$ds_name" == "None" ] ; then ds_name=$ds_id ; else ds_name=$ds_name ; fi
 
-#step2
+# Run the Neurobagel CLI
 docker run --rm -v ${workdir}:${workdir} neurobagel/bagelcli pheno --pheno ${workdir}/participants.tsv --dictionary ${workdir}/participants.json --output ${workdir} --name "$ds_name" --portal $ds_portal
 docker run --rm -v ${workdir}:${workdir} neurobagel/bagelcli bids --jsonld-path ${workdir}/pheno.jsonld  --bids-dir ${workdir} --output ${workdir}
 cp ${workdir}/pheno_bids.jsonld ${out}
