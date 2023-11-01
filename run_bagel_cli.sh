a1febb3fd2bede00eb411f8063c58d1de4739770
Alyssa Dai <alyssa.ydai@gmail.com>
2023-11-01 11:42:22 -0400

    update CLI invocations

--- run_bagel_cli.sh
+++ run_bagel_cli.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/bash
 
-# make sure we're up to date
-docker pull neurobagel/bagelcli:latest
-
 ldin=inputs/openneuro-jsonld/
 ldout=outputs/openneuro-jsonld/
 
@@ -23,13 +20,12 @@ fi
 
 echo $ds "$ds_name"
 if [ ! -e ${out} ]; then
-
     echo Checking data dictionary for descriptions!
     python3 add_description.py ${workdir}/participants.json
 
-    echo bagel pheno --pheno ${workdir}/participants.tsv --dictionary ${workdir}/participants.json --output ${workdir} --name "$ds_name" --portal $ds_portal
-    docker run -v ${workdir}:${container_dir} neurobagel/bagelcli:latest pheno --pheno ${container_dir}/participants.tsv --dictionary ${container_dir}/participants.json --output ${container_dir} --name "$ds_name" --portal $ds_portal
-    docker run -v ${workdir}:${container_dir} neurobagel/bagelcli:latest bids --jsonld-path ${container_dir}/pheno.jsonld  --bids-dir ${container_dir} --output ${container_dir}
+    echo bagel pheno --pheno ${workdir}/participants.tsv --dictionary ${workdir}/participants.json --output ${workdir}/pheno.jsonld --name "$ds_name" --portal $ds_portal
+    docker run --rm -v ${workdir}:${container_dir} neurobagel/bagelcli:latest pheno --pheno ${container_dir}/participants.tsv --dictionary ${container_dir}/participants.json --output ${container_dir}/pheno.jsonld --name "$ds_name" --portal $ds_portal
+    docker run --rm -v ${workdir}:${container_dir} neurobagel/bagelcli:latest bids --jsonld-path ${container_dir}/pheno.jsonld --bids-dir ${container_dir} --output ${container_dir}/pheno_bids.jsonld
 
     echo Resetting dataset to HEAD
     git -C ${workdir} checkout HEAD -- participants.json
